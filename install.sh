3d2092a61fcb75be57b7c76c0d311256d2141871
Chinar Joshi <chinarjoshi7@gmail.com>
2021-10-06 15:30:45 -0400

    Wed Oct  6 03:30:45 PM EDT 2021

--- install.sh
+++ install.sh
@@ -1,18 +1,20 @@
-#! /bin/zsh
+#!/bin/sh
 
 DOTFILES=$HOME/dotfiles
 [[ -z $XDG_CONFIG_HOME ]] && XDG_CONFIG_HOME=$HOME/.config
 
+[ ! $(command -v zsh) ] || echo "Z shell not installed." 1>&2 && exit 1
+
 git clone https://github.com/chinarjoshi/dotfiles.git $DOTFILES
 cd $DOTFILES
 
-ln -sv $DOTFILES/emacs $HOME/.doom.d
-
 
 # All directories belong in XDG_CONFIG_HOME except for the following
-exclude=('boot' 'emacs' 'libinput')
+exclude=('boot' 'emacs' 'libinput' 'X11')
 
-for config in 'nvim' 'alacritty' 'i3'; do
+#ls -d $DOTFILES/*/ | xargs -n 1 basename
+
+for config in ; do
     ln -sv $DOTFILES/$config $XDG_CONFIG_HOME/$config
 done
 
@@ -20,6 +22,10 @@ for X in $(ls -A $DOTFILES/X11); do
     ln -sv $DOTFILES/X11/$X $HOME/$X
 done
 
+ln -sv $DOTFILES/emacs $HOME/.doom.d
+ln -sv $DOTFILES/libinput/libinput-gestures.conf $XDG_CONFIG_HOME/libinput-gestures.conf
 sudo cp $DOTFILES/boot /boot/loader
+sudo ln -sv $DOTFILES/libinput/libinput.conf 
+sudo ln -sv $DOTFILES/libinput/libinput.conf 
 
 echo "Bootstrapping successful for $USER"
