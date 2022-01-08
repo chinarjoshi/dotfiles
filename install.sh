6e82dfd68a45a12da13d9c15dae30d96aee074a6
Chinar Joshi <chinarjoshi7@gmail.com>
2022-01-08 03:43:48 -0500

    Sat Jan  8 03:43:48 AM EST 2022

--- install.sh
+++ install.sh
@@ -8,21 +8,12 @@ DOTFILES=$HOME/dotfiles
 git clone https://github.com/chinarjoshi/dotfiles.git $DOTFILES
 
 # All directories belong in XDG_CONFIG_HOME except for the following
-exclude='boot emacs libinput X11'
-
+exclude='wallpapers'
 for dir in $(ls -d $DOTFILES/*/ | xargs -n 1 basename); do
     [[ $exclude =~ $dir ]] && 
         ln -sv $DOTFILES/$dir $XDG_CONFIG_HOME/$dir
 done
-
-for X in $(ls -A $DOTFILES/X); do
-    ln -sv $DOTFILES/X/$X $HOME/$X
-done
-
-ln -sv $DOTFILES/emacs $HOME/.doom.d
-ln -sv $DOTFILES/libinput/libinput-gestures.conf $XDG_CONFIG_HOME/libinput-gestures.conf
-sudo cp $DOTFILES/boot /boot/loader
-sudo ln -sv $DOTFILES/libinput/libinput.conf /etc/X11/xorg.conf.d/10-libinput.conf
-sudo ln -sv $DOTFILES/libinput/libinput.conf /etc/X11/xorg.conf.d/20-mtrack.conf
+ln -sv $DOTFILES/libinput-gestures.conf $XDG_CONFIG_HOME/libinput-gestures.conf
+ln -sv $DOTFILES/.doom.d $HOME/.doom.d
 
 echo "Bootstrapping successful for $USER"
