d7bd3bedbde903dbe18b3e193e9f6ede5556ea1a
Chinar Joshi <chinarjoshi7@gmail.com>
2022-01-09 02:04:45 -0500

    Sun Jan  9 02:04:45 AM EST 2022

--- zsh/custom/bindkey.zsh
+++ zsh/custom/bindkey.zsh
@@ -1,14 +1,14 @@
 # Command to function
 clear_and_ls() { clear && l }
-git_finish() { gf }
-git_status() { gs }
+#git_finish() { gf }
+#git_status() { gs }
 
 # Function to widget
 zle -N clear_and_ls
-zle -N git_finish
-zle -N git_status
+#zle -N git_finish
+#zle -N git_status
 
 # Widget to shortcut
 bindkey '^k' clear_and_ls
-bindkey '^g^g' git_finish
-bindkey '^g^s' git_status
+#bindkey '^g^g' git_finish
+#bindkey '^g^s' git_status
