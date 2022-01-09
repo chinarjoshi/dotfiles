f07ccb7a74d58045a0bea74ba76f30a93ce245e5
Chinar Joshi <chinarjoshi7@gmail.com>
2022-01-09 02:03:32 -0500

    Sun Jan  9 02:03:32 AM EST 2022

--- zsh/custom/bindkey.zsh
+++ zsh/custom/bindkey.zsh
@@ -1,12 +1,14 @@
-declare -A names=(
-    'clear_and_ls' 'clear && l'
-    'git_finish'   'gf'
-    'git_status'   'gs' )
-for name command in "${(@kv)names[@]}"; do
-    $name() { $command }
-    zle -N $name
-done
+# Command to function
+clear_and_ls() { clear && l }
+git_finish() { gf }
+git_status() { gs }
 
+# Function to widget
+zle -N clear_and_ls
+zle -N git_finish
+zle -N git_status
+
+# Widget to shortcut
 bindkey '^k' clear_and_ls
 bindkey '^g^g' git_finish
 bindkey '^g^s' git_status
