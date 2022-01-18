124272494776d934337f7a01bdaeaac42a7af53f
Chinar Joshi <chinarjoshi7@gmail.com>
2022-01-17 22:17:01 -0500

    Mon Jan 17 10:17:01 PM EST 2022

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ wifi() {
     [[ $(ip link | grep -c wlan) ]]
     cat <<- EOF > /var/lib/iwd/${NETWORK_SSID}.psk
 	[Security]
-	Passphrase=6080700101
+	Passphrase=$NETWORK_PASSWD
 EOF
     systemctl restart iwd
     iwctl station wlan0 scan
@@ -43,38 +43,68 @@ time_lang() {
     echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
     locale-gen
     echo 'LANG=en_US.UTF-8' > /etc/locale.conf
-    echo 'c' > /etc/hostname
+    echo $HOSTNAME > /etc/hostname
+    mkdir -p /var/lib/iwd/
+    cat <<- EOF > /var/lib/iwd/${NETWORK_SSID}.psk
+	[Security]
+	Passphrase=$NETWORK_PASSWD
+EOF
+    cat <<- EOF > /etc/systemd/network/25-wireless.network
+	[Match]
+	Name=wlan0
+	
+	[Network]
+	DHCP=yes
+EOF
     mkinitcpio -P
     echo 'root:fdsa' | chpasswd
 }
 
-
 users_systemd() {
     useradd -m c
     echo 'c ALL=(ALL) NOPASSWD: ALL' | EDITOR='tee -a' visudo
-    systemctl enable systemd-networkd systemd-resolved iwd
+    systemctl enable systemd-networkd systemd-resolved systemd-timesyncd iwd
 }
 
 yay_install() {
     su c -c "git clone https://aur.archlinux.org/yay /home/c/yay \
-	       && cd /home/c/yay && makepkg -si && echo $AUR_PKG | xargs yay -S"
+	       && cd /home/c/yay && makepkg -si --noconfirm && echo $AUR_PKG | xargs yay -S --noconfirm"
     if $IS_LAPTOP; then
-        su c -c "echo $LAPTOP_PKG | xargs yay -S"
+        su c -c "echo $LAPTOP_PKG | xargs yay -S --noconfirm"
     fi
 }
 
 boot() {
     bootctl install
-    echo $LOADER > /boot/loader/loader.conf
-    echo $BOOT_ENTRY > /boot/loader/entries/arch.conf
+    cat <<- EOF > /boot/loader/loader.conf
+	default arch.conf
+	timeout false
+	console-mode max
+	editor no
+EOF
+    cat <<- EOF > /boot/loader/entries/arch.conf
+	title ChinArch Linux
+	linux /vmlinuz-linux
+	initrd /${CPU_TYPE}-ucode.img
+	initrd /initramfs-linux.img
+	options root=PARTUUID=$(blkid -s PARTUUID -o value /dev/sda2) rw quiet $($IS_LAPTOP && echo acpi_osi=!Darwin)
+EOF
+    mkdir -p /etc/systemd/system/getty@tty1.service.d/
+    cat <<- EOF > /etc/systemd/system/getty@tty1.service.d/override.conf
+	[Service]
+	ExecStart=
+	ExecStart=-/usr/bin/agetty --autologin c --noclear %I $TERM
+EOF
 }
 
 symlinks() {
+    HOME=/home/c
     DOTFILES=$HOME/dotfiles
     XDG_CONFIG_HOME=$HOME/.config
-    mkdir $HOME/.config
+    mkdir -p $HOME/.config
 
     git clone https://github.com/chinarjoshi/dotfiles.git $DOTFILES
+    chown -R c $DOTFILES
 
     for dir in $(ls -d $DOTFILES/*/ | xargs -n 1 basename); do
         ln -sv $DOTFILES/$dir $XDG_CONFIG_HOME/$dir
@@ -83,38 +113,69 @@ symlinks() {
     ln -sv $DOTFILES/libinput-gestures.conf $XDG_CONFIG_HOME/libinput-gestures.conf
     ln -sv $DOTFILES/.doom.d $HOME/.doom.d
 
-    echo "Bootstrapping successful for $USER"
+    chsh -s /bin/zsh c
 }
 
 alsa_config() {
-    amixer sset Master unmute
-    amixer sset Speaker unmute
-    amixer sset Headphone unmute
-    echo $PULSE_CONFIG > /home/c/.config/pulse/daemon.conf
-    echo $ALSA_CONFIG > /etc/asound.conf
+    #amixer sset Master unmute
+    #amixer sset Speaker unmute
+    #amixer sset Headphone unmute
+    mkdir -p /home/c/.config/pulse/
+    cat <<- EOF > /home/c/.config/pulse/daemon.conf
+	default-sample-format = float32le
+	default-sample-rate = 48000
+	alternate-sample-rate = 44100
+	default-sample-channels = 2
+	default-channel-map = front-left,front-right
+	default-fragments = 2
+	default-fragment-size-msec = 125
+	resample-method = soxr-vhq
+	enable-lfe-remixing = no
+	high-priority = yes
+	nice-level = -11
+	realtime-scheduling = yes
+	realtime-priority = 9
+	rlimit-rtprio = 9
+	daemonize = no
+EOF
+    chown c /home/c/.config/pulse/daemon.conf
+    cat <<- EOF > /etc/asound.conf
+	# Use PulseAudio plugin hw
+	pcm.!default {
+	   type plug
+	   slave.pcm hw
+	}
+EOF
 }
 
 caps_to_escape() {
-    echo $CAPS_CONFIG > /etc/interception/udevmon.d/caps2esc.yaml
-    systemctl enable udevmon
+    mkdir -p /etc/interception/udevmon.d/
+    cat <<- EOF > /etc/interception/udevmon.d/caps2esc.yaml
+	- JOB: \"intercept -g $DEVNODE | caps2esc | uinput -d $DEVNODE\"
+	  DEVICE:
+	    EVENTS:
+	      EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
+EOF
+    #systemctl enable udevmon
 }
 
 # ---------------- CONFIGURATION VARIABLES ---------------
 
-IS_LAPTOP=true
+HOSTNAME=Desktop
+IS_LAPTOP=false
 NETWORK_SSID=ATT-phanas
+NETWORK_PASSWD=6080700101
 INSTALL_DISK=/dev/sda
 SWAP_SIZE=10G
+CPU_TYPE=amd
 FDISK_CMD='g\nn\n1\n\n+300MiB\nn\n2\n\n\nt\n1\n1\nt\n2\n20\nw\n'
 
-# --------------- PACKAGES ----------------
-
-MAIN_PKG='
+MAIN_PKG="
 linux
 linux-firmware
 base
 base-devel
-amd-ucode
+${CPU_TYPE}-ucode
 gcc
 git
 zsh
@@ -154,7 +215,7 @@ bat
 ntfs-3g
 exfat-utils
 python
-python-pip'
+python-pip"
 
 LAPTOP_PKG='libinput-gestures
 light
@@ -167,55 +228,6 @@ discord_arch_electron
 clipman
 spotify'
 
-# --------------------- FILES ----------------------
-
-# ATT-phanas.
-
-# loader.conf
-LOADER="default arch.conf
-timeout false
-console-mode max
-editor no"
-
-# arch.conf 
-BOOT_ENTRY="title PenixOS
-linux /vmlinuz-linux
-initrd /intel-ucode.img
-initrd /initramfs-linux.img
-options root=PARTUUID=$(blkid -s PARTUUID -o value /dev/sda2) rw quiet acpi_osi=!Darwin"
-
-# pulse/daemon.conf
-PULSE_CONFIG="
-default-sample-format = float32le
-default-sample-rate = 48000
-alternate-sample-rate = 44100
-default-sample-channels = 2
-default-channel-map = front-left,front-right
-default-fragments = 2
-default-fragment-size-msec = 125
-resample-method = soxr-vhq
-enable-lfe-remixing = no
-high-priority = yes
-nice-level = -11
-realtime-scheduling = yes
-realtime-priority = 9
-rlimit-rtprio = 9
-daemonize = no"
-
-# asound.conf
-ALSA_CONFIG="
-# Use PulseAudio plugin hw
-pcm.!default {
-   type plug
-   slave.pcm hw
-}"
-
-CAPS_CONFIG="
-- JOB: \"intercept -g $DEVNODE | caps2esc | uinput -d $DEVNODE\"
-  DEVICE:
-    EVENTS:
-      EV_KEY: [KEY_CAPSLOCK, KEY_ESC]"
-
 case $1 in
     -chroot) 
       time_lang
@@ -225,6 +237,7 @@ case $1 in
       symlinks
       alsa_config
       caps_to_escape
+      echo "\n---------------------------\nDone :)"
     ;;
     *)
       wifi
