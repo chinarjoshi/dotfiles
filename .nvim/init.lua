3f6eac17f93a8f5834cd1291d6012ceb3f8407fd
Chinar Joshi <chinarjoshi7@gmail.com>
2022-02-06 22:51:31 -0500

    Sun Feb  6 10:51:31 PM EST 2022

---
+++ .nvim/init.lua
@@ -0,0 +1,19 @@
+-- Core files
+core = {
+  'settings',
+  'keymaps',
+  'plugins'
+}
+-- Plugins with their own config file
+long = {
+  'nvim-tree',
+  'feline',
+  'indent-blankline',
+  'treesitter',
+  'nvim-cmp',
+  'nvim-lspconfig',
+  'vista',
+  'others'
+}
+for _, file in ipairs(core) do require(file) end
+for _, file in ipairs(long) do require('config.' .. file) end
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,19 +1,31 @@
--- Core files
-core = {
-  'settings',
-  'keymaps',
-  'plugins'
+local present, impatient = pcall(require, "impatient")
+
+if present then
+   impatient.enable_profile()
+end
+
+local core_modules = {
+   "core.options",
+   "core.autocmds",
+   "core.mappings",
 }
--- Plugins with their own config file
-long = {
-  'nvim-tree',
-  'feline',
-  'indent-blankline',
-  'treesitter',
-  'nvim-cmp',
-  'nvim-lspconfig',
-  'vista',
-  'others'
-}
-for _, file in ipairs(core) do require(file) end
-for _, file in ipairs(long) do require('config.' .. file) end
+
+for _, module in ipairs(core_modules) do
+   local ok, err = pcall(require, module)
+   if not ok then
+      error("Error loading " .. module .. "\n\n" .. err)
+   end
+end
+
+-- non plugin mappings
+require("core.mappings").misc()
+
+-- check if custom init.lua file exists
+if vim.fn.filereadable(vim.fn.stdpath "config" .. "/lua/custom/init.lua") == 1 then
+   -- try to call custom init, if not successful, show error
+   local ok, err = pcall(require, "custom")
+   if not ok then
+      vim.notify("Error loading custom/init.lua\n\n" .. err)
+   end
+   return
+end
