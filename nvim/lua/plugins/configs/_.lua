476dc9297a6636197b3d01256970e6f57046cbb8
Chinar Joshi <chinarjoshi7@gmail.com>
2022-02-28 18:29:48 -0500

    Testing

--- nvim/lua/plugins/configs/_.lua
+++ nvim/lua/plugins/configs/_.lua
@@ -1,76 +1,56 @@
-return {
-    ['nvim-autopairs'] = require('nvim-autopairs').setup{fast_wrap = {}},
-    lsp_signature = require('lsp_signature').setup{
-          bind = true,
-          --doc_lines = 1,
-          floating_window = false,
-          fix_pos = true,
-          hint_enable = true,
-          hint_prefix = ' ',
-          hint_scheme = 'String',
-          hi_parameter = 'Search',
-          max_height = 22,
-          max_width = 120,
-          handler_opts = {
-            border = 'single',
-          },
-          zindex = 200,
-          padding = '',
-        },
-    gitsigns = require('gitsigns').setup {
-      signs = {
-        add = { hl = 'DiffAdd', text = '│', numhl = 'GitSignsAddNr' },
-        change = { hl = 'DiffChange', text = '│', numhl = 'GitSignsChangeNr' },
-        delete = { hl = 'DiffDelete', text = '│', numhl = 'GitSignsDeleteNr' },
-        topdelete = { hl = 'DiffDelete', text = '‾', numhl = 'GitSignsDeleteNr' },
-        changedelete = { hl = 'DiffChangeDelete', text = '~', numhl = 'GitSignsChangeNr' },
-      },
-    },
-    orgmode = {
-        require('orgmode').setup {
-          org_agenda_files = { '~/my-orgs/**/*' },
-          org_default_notes_file = '~/org/notes.org',
-        },
-    },
-    trouble = {
-        require('trouble').setup {
-      height = 20,
-      width = 50
-    },
-    },
-    ['todo-comments'] = {
-        require('todo-comments').setup {
-      highlight = {
-        keyword = 'wide'
-      }
-    },
-    },
-    toggleterm = require('toggleterm').setup { size = 15 },
-    neogit = require('neogit').setup(),
-    project_nvim = require('project_nvim').setup()
-    sniprun = require('sniprun').setup()
-}
+local M = {}
 
-for _, name in ipairs({
-    'nvim-autopairs',
-    'lsp_signature',
-    'gitsigns',
-    'trouble',
-    'orgmode',
-    'todo-comments',
-    'neogit',
-    'project_nvim',
-    'sniprun',
-    'toggleterm',
-}) do
-    local ok, plugin = pcall(require, name)
-    if not ok then
-        return
-    end
-    plugin.setup(configs[name] or {})
+M['nvim-autopairs'] = function()
+    require('nvim-autopairs').setup { fast_wrap = {} }
+    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+    require('cmp').event:on('confirm_done', cmp_autopairs.on_confirm_done())
+end
+M.lsp_signature = require('lsp_signature').setup {
+  bind = true,
+  --doc_lines = 1,
+  floating_window = false,
+  fix_pos = true,
+  hint_enable = true,
+  hint_prefix = ' ',
+  hint_scheme = 'String',
+  hi_parameter = 'Search',
+  max_height = 22,
+  max_width = 120,
+  handler_opts = {
+    border = 'single',
+  },
+  zindex = 200,
+  padding = '',
+}
+M.gitsigns = require('gitsigns').setup {
+  signs = {
+    add = { hl = 'DiffAdd', text = '│', numhl = 'GitSignsAddNr' },
+    change = { hl = 'DiffChange', text = '│', numhl = 'GitSignsChangeNr' },
+    delete = { hl = 'DiffDelete', text = '│', numhl = 'GitSignsDeleteNr' },
+    topdelete = { hl = 'DiffDelete', text = '‾', numhl = 'GitSignsDeleteNr' },
+    changedelete = { hl = 'DiffChangeDelete', text = '~', numhl = 'GitSignsChangeNr' },
+  },
+}
+M.orgmode = require('orgmode').setup {
+  org_agenda_files = { '~/my-orgs/**/*' },
+  org_default_notes_file = '~/org/notes.org',
+}
+M.trouble = require('trouble').setup {
+  height = 20,
+  width = 50
+}
+M['todo-comments'] = require('todo-comments').setup {
+  highlight = {
+    keyword = 'wide'
+  }
+}
+M.toggleterm = require('toggleterm').setup { size = 15 }
+M.neogit = require('neogit').setup()
+M.project_nvim = require('project_nvim').setup()
+M.sniprun = require('sniprun').setup()
+M.luasnip = function()
+    require('luasnip.loaders.from_vscode').load()
+    require('luasnip').config.set_config({ history = true, updateevents = 'TextChanged,TextChangedI' })
 end
 
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-require('cmp').event:on('confirm_done', cmp_autopairs.on_confirm_done())
-require('luasnip.loaders.from_vscode').load()
-require('luasnip').config.set_config({ history = true, updateevents = 'TextChanged,TextChangedI' })
+return M
