1dde28ba8a1f09b9d2ea8b0f7c9dc30a93d0ddd7
Chinar Joshi <chinarjoshi7@gmail.com>
2022-03-28 14:03:46 -0400

    Update waybar

---
+++ waybar/modules/github.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+token=`cat ~/token.txt`
+count=`curl -u chinarjoshi:${token} https://api.github.com/notifications | jq '. | length'`
+
+if [[ "$count" != "0" ]]; then
+    echo '{"text":'$count',"tooltip":"$tooltip","class":"$class"}'
+fi
