e94ba1814baf919846f6aa4922b40e4e8d71f062
Chinar Joshi <chinarhjoshi@gmail.com>
2022-11-07 02:24:00 -0500

    Remove network part of init

--- gentoo/setup.sh
+++ gentoo/setup.sh
@@ -1,12 +1,3 @@
-cat > /etc/wpa_supplicant/wpa_supplicant.conf <<- EOF
-network={
-    ssid="eduroam"
-    key_mgmt=WPA-EAP
-    identity="cjoshi44@gatech.edu"
-    password="$(cat ~/passwd.sh)"
-}
-EOF
-
 mount /dev/nvme0n1p2 /mnt/gentoo
 mount /dev/nvme0n1p1 /mnt/gentoo/boot
 mount --types proc /proc /mnt/gentoo/proc
